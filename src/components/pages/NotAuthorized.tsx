bf005f57e0a37402543b559cdd5c635ac178a949
deepakSingh023 <dsinghrana71@gmail.com>
2025-08-22 22:02:40 +0530

    fixing bugs

--- src/components/pages/NotAuthorized.tsx
+++ src/components/pages/NotAuthorized.tsx
@@ -7,7 +7,8 @@ export default function NotAuthorizedPage() {
   const router = useNavigate();
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
+    <div className="flex items-center justify-center min-h-screen h-screen bg-cover bg-center bg-gray-50 px-4"
+    style={{ backgroundImage: "url('/public/bgImage.jpg')" }}>
       <div className="bg-white shadow-lg rounded-2xl p-8 max-w-md w-full text-center border border-gray-200">
         <h1 className="text-2xl font-bold text-red-600 mb-4">
           Access Denied
